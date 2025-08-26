7c9e5547c43b20600a9a6ce9a91a6b16ea22eb69
ryota37 <ryota.nakahama@access-company.com>
2025-08-26 09:12:57 +0900

    エンジニアを実装

--- scenes/main_scene/MainScene.cs
+++ scenes/main_scene/MainScene.cs
@@ -4,7 +4,9 @@ using System;
 public partial class MainScene : Node2D
 {
 	private Label _number_of_lines;
-	private int _count = 0;
+	private double _count = 0;
+	
+	private double _number_of_engineer = 0.0;
 	
 	public override void _Ready()
 	{
@@ -14,10 +16,16 @@ public partial class MainScene : Node2D
 	
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("KeyEnter"))
+		if (Input.IsActionJustPressed("KeyA"))
 		{
-			_count++;
-			_number_of_lines.Text = $"Number of lines: {_count}";
+			_count = _count + 10.0;
 		}
+		
+		if (Input.IsActionJustPressed("KeyJ"))
+		{
+			_number_of_engineer = _number_of_engineer + 0.01;
+		}		
+		_count = _count + _number_of_engineer;
+		_number_of_lines.Text = $"Number of lines: {Math.Floor(_count)}";
 	}
 }
